cee4bdccbe7f3b18047edd045527f277fc5c25e6
Tim Thomas <tim@buyplaytix.com>
2011-11-07 11:20:05 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qrcode::Application.config.secret_token = '87d9f13840344c48c774957c67d6a47babacc3c3fb5516fd94ad0b6d50273aec22ca63b2c4e1d115bc1f8dfae5e60ce4676cc30b8b67be10b805f081f1a5def7'
