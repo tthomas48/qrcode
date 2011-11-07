dd007468be373c379aad46553586a6e2c7f1ac5f
Tim Thomas <tim@buyplaytix.com>
2011-11-07 11:36:19 -0600

    Added static test page

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+set :application, "qrcode"
+set :repository,  "https://github.com/tthomas48/qrcode"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :deploy_to, "/var/www/rails/qrcode"
+server "loadedguntheory.com", :app, :web, :db, :primary => true
+
+set :use_sudo, false
+
+#role :web, "loadedguntheory.com"                          # Your HTTP server, Apache/etc
+#role :app, "loadedguntheory.com"                          # This may be the same as your `Web` server
+#role :db,  "loadedgunteory.com", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
