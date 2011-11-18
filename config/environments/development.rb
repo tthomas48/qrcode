5712c0f0d0f2b84976269ed037d0a3346bca157f
Tim Thomas <tim@buyplaytix.com>
2011-11-18 09:32:01 -0600

    turned off mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Qrcode::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  #config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
 
   # Print deprecation notices to the Rails logger
@@ -24,6 +24,6 @@ Qrcode::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  config.action_mailer.default_url_options = { :host => 'localhost:25' }
+  #config.action_mailer.default_url_options = { :host => 'mail.buyplaytix.com:25' }
 end
 
